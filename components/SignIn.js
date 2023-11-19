fbbbaafdec2ff948c07058d5da35bfb7c4bc849f
chiuinggum <yuntingchiu@gapp.nthu.edu.tw>
2023-11-19 20:38:42 +0800

    feat(auth): add sign in page

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,8 +1,21 @@
 export default function SignIn() {
     return (
-        <div>
-            <h1>Sign In</h1>
-            
+        <div className="w-96 border shadow-lg rounded">
+            <h1 className="m-5">Sign In</h1>
+            <form>
+                <span className="flex mx-5 my-3 flex-col">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="usernameOrEmail">Username/Email</label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="usernameOrEmail" type="text" placeholder="Username or Email"/>
+                </span>
+                <span className="flex mx-5 my-3 flex-col">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+                </span>
+                <span>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5 my-3 focus:outline-none focus:shadow-outline" type="button">Sign Up</button>
+                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">Forgot Password?</a>
+                </span>
+            </form>
         </div>
     );
 };
---
+++ pages/signin/index.js
@@ -0,0 +1,9 @@
+import SignIn from "@/components/SignIn";
+
+export default function SignInPage() {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <SignIn/>
+        </div>
+    );
+}
