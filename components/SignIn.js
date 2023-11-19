6d1b77a7823165d3d50b4a55e455b9ae2c151246
chiuinggum <yuntingchiu@gapp.nthu.edu.tw>
2023-11-19 20:51:09 +0800

    feat(auth): link sign up page with sign in page

--- components/SignIn.js
+++ components/SignIn.js
@@ -11,8 +11,11 @@ export default function SignIn() {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
                     <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
                 </span>
+                <span className="flex mx-5 my-1 text-sm">
+                    <p>Don't have an account yet? <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="./signup">Create one</a>.</p>
+                </span>
                 <span>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5 my-3 focus:outline-none focus:shadow-outline" type="button">Sign Up</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5 my-3 focus:outline-none focus:shadow-outline" type="button">Sign In</button>
                     <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">Forgot Password?</a>
                 </span>
             </form>
--- components/SignUp.js
+++ components/SignUp.js
@@ -19,6 +19,9 @@ export default function SignUp() {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Confirm Password</label>
                     <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
                 </span>
+                <span className="flex mx-5 my-1 text-sm">
+                    <p>Already have an account? <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="./signin">Log in</a>.</p>
+                </span>
                 <span>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5 my-3 focus:outline-none focus:shadow-outline" type="button">Sign Up</button>
                 </span>
