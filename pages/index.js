e217286a53c78b70ebe2edcfe7ef24399aef84f3
chiuinggum <yuntingchiu@gapp.nthu.edu.tw>
2023-11-21 15:41:31 +0800

    fix(home): change 'class' with 'className'

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import TripInput from '../components/TripInput'
 import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
 import { Inter } from 'next/font/google'
+import NavBar from '@/components/NavBar';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      <AccountCircleTwoToneIcon />
-      <TripInput />
+      <NavBar/>
     </div>
   );
 }
