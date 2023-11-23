5846e6ba4e945b3a8f54c40461a6e486f37ddd40
chiuinggum <yuntingchiu@gapp.nthu.edu.tw>
2023-11-23 15:12:37 +0800

    feat(map): add sidebar to map page

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,9 +1,11 @@
 import CreateMap from '@/components/CreateMap';
+import MapSidebar from '@/components/MapSidebar';
 
 export default function Map() {
     return (
         <div>
-            <CreateMap/>
+            {/* <CreateMap/> */}
+            <MapSidebar/>
         </div>
     );
 };
